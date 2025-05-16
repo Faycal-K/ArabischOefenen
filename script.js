83bb10cab36177cbebbbca3fed088be2d3d16d4a
Faycal-K <faycalkerroue1999@gmail.com>
2025-05-16 20:53:38 +0200

    Update script.js
    Added more words.

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-const arabicWords = ["بَيْتٌ", "مَسْجِدٌ", "مِلْعَقَةٌ"];
-const dutchWords = ["Huis", "Moskee", "Lepel"];
+const arabicWords = [
+  "أَحْمَرَ", "أَزْرَقَ", "أَخْضَرَ", "أَسْوَدَ", "أَصْفَرَ", "أَبْيَضَ",
+  "بَغْدَادُ", "جِدَّةُ", "كَوْنٌ", "دَقِيقَةٌ", "دَقَائِقُ", "مِفْتَاحٌ", "مَفَاتِيحُ", "قَالَ", "قَالَتْ",
+  "إِصْطَنْبُولُ", "وَاشِنْطُنْ", "الطَّائِفُ",
+  "كُلُّ", "كُلُّهُمْ", "كُلُّكُمْ", "كُلُّنَا", "ٱلْبَلَدُ", "مُخْتَلِفٌ", "ٱلْحَافِلَةُ", "مِنْهَا", "أُورُوبَّا", "يُوغُسْلَافِيَا",
+  "ٱلْيَوْمُ", "ٱلثَّمَنُ", "ٱلنِّصْفُ", "قَدِيمٌ", "ٱلْحَقِيبَةُ", "ٱلرُّكَّابُ", "ٱلسُّؤَالُ", "كَلِمَةٌ", "كَلِمَاتٌ", "مَجَلَّةٌ", "حُرُوفٌ",
+  "إِنْدُونِيسِيَا", "غُرْفَةٌ", "دَرْسٌ", "عَمٌّ",
+  "ٱلنَّهْرُ", "ٱلْبَحْرُ", "ٱلْفُنْدُقُ", "ٱلطَّائِرَةُ",
+  "ٱلشَّرِكَةُ", "مُدِيرُ ٱلشَّرِكَةِ", "رَخِيصٌ", "يَابَانِيَّةٌ", "قَمِيصٌ",
+  "ٱلْعَجَلَةُ", "ٱلْحَفْلُ", "ٱلنَّافِذَةُ", "ٱلسَّنَةُ", "ٱلْحَيُّ", "ٱلْمِسْطَرَةُ", "ٱلرَّكْعَةُ"
+];
+const dutchWords = [
+  "Rood", "Blauw", "groen", "Zwart", "geel", "Wit",
+  "Baghdad", "Jeddah", "kom (object)", "minuut", "minuten", "sleutel", "sleutels", "zei (hij)", "zei (zij)",
+  "Istanbul", "Washington", "Ta'if",
+  "Elke", "Elk van hun", "Elke van jullie", "Elke van ons", "De land", "Verschillend", "De bus", "Van hen", "Europa", "Joegoslavië",
+  "De dag", "De prijs", "De helft", "Oud", "De tas (zak)", "De passagier", "De vraag", "Het woord", "Woorden", "Tijdschrift", "Letter(s)",
+  "Indonesië", "Kamer", "Les", "Oom",
+  "Het rivier", "De zee", "De hotel", "De vliegtuig",
+  "Het bedrijf", "De directeur van het bedrijf", "Goedkoop", "Japans", "Blouse/garment/shirt",
+  "Het wiel", "Het feest", "Het raam", "Het jaar", "De wijk", "De liniaal", "De gebedsaanheid"
+];
 
 const randIndex = Math.floor(Math.random() * arabicWords.length);
 
